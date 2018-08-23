047a0e4beb7eb3127f90e9454221018c4963950d
cdachpcie <36476204+cdachpcie@users.noreply.github.com>
2018-08-23 10:22:48 +0530

    Update dialog.php

--- plugins/Codiad-Terminal-master/dialog.php
+++ plugins/Codiad-Terminal-master/dialog.php
@@ -1,9 +1,7 @@
 <?php
 
     /*
-    *  Copyright (c) Codiad & Kent Safranski (codiad.com), distributed
-    *  as-is and without warranty under the MIT License. See 
-    *  [root]/license.txt for more. This information must remain intact.
+    *  Copyright (c) C-DAC
     */
 
 
