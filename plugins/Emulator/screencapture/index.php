b09f4b58ec8c58823255d48f35ff2187fb2d176e
cdachpcie <solsupport@cdac.in>
2018-09-27 15:33:39 +0530

    code added to capture output

---
+++ plugins/Emulator/screencapture/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Screenshot using html2canvas</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://html2canvas.hertzen.com/dist/html2canvas.min.js"></script>
+</head>
+<body>
+
+	<h1 style="text-align: center;">Screenshot</h1>
+	<div id="capture" style="padding: 10px; background: #f5da55">
+		<h4 style="color: #000; ">Hello!!</h4>
+	</div>
+	<div style="margin-top: 50px; text-align: center;">
+		<button id="take_screenshoot">Take Screenshot</button>
+	</div>
+
+	<script type="text/javascript">
+		var dataURL = {};
+		$('#take_screenshoot').click(function(){
+			html2canvas(document.querySelector("#capture")).then(canvas => {
+				document.body.appendChild(canvas);
+
+				//console.log(canvas.toDataURL());
+				dataURL = canvas.toDataURL();
+				post_data(dataURL);  	
+
+			});
+
+		});
+
+
+		function post_data(imageURL){
+		console.log(imageURL);
+		$.ajax({
+			url: "save_data.php",
+			type: "POST",
+			data: {image: imageURL},
+			dataType: "html",
+			success: function(data,status) {
+				alert(data);
+				//alert('Success!!');
+				location.reload();
+			}
+		});
+		
+	}
+
+</script>
+</body>
+</html>
